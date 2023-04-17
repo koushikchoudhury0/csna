00c432739889f1d79bc6db914b4f2073f93caf31
Koushik <koushikchoudhury0@gmail.com>
2023-04-17 18:10:29 +0530

    Cleaned up user controller

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -1,13 +1,7 @@
 import { Router } from 'express';
 import { codes as HTTPCodes } from '../../constants/http.constants.js';
 import { getUserFromRequest } from '../../utils/auth.utils.js';
-import { todoModel } from '../models/todo.model.js';
-import { createTodo, getTodosByUserId, handleTodoDelete, handleTodoUpdate, updateTodo } from '../helpers/todo.helper.js';
-import { addTodoValidator, deleteTodoValidator, getOtherTodoValidator, getTodoValidator, updateTodoValidator } from '../validators/todo.validator.js';
-import { handleWriteTodoError, handleGenericError } from '../helpers/error.helper.js';
-import { pageSanitizer } from '../sanitizers/page.sanitizer.js';
-import { MongoDeleteResult, MongoUpdateResult } from '../../types/mongo-results.interface.js';
-import { CustomError } from '../../types/custom-error.class.js';
+import { handleGenericError } from '../helpers/error.helper.js';
 import { usersModel } from '../models/users.model.js';
 const { ERROR } = HTTPCodes;
 
